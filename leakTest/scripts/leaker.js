d96c5a2760363beb2330e00630b35e32e7037824
yupeiyong <2758318132@qq.com>
2017-09-26 22:53:37 +0800

    js内存泄漏测试

---
+++ leakTest/scripts/leaker.js
@@ -0,0 +1,31 @@
+﻿//var Leaker = function () { };
+//Leaker.prototype = {
+//    init: function () {
+
+//    }
+//};
+var Leaker = function () { };
+
+Leaker.prototype = {
+    init: function () {
+        this._interval = null;
+        this.start();
+    },
+
+    start: function () {
+        var self = this;
+        this._interval = setInterval(function () {
+            self.onInterval();
+        }, 100);
+    },
+
+    destroy: function () {
+        if (this._interval !== null) {
+            clearInterval(this._interval);
+        }
+    },
+
+    onInterval: function () {
+        console.log("Interval");
+    }
+};
---
+++ leakTest/scripts/main.js
@@ -0,0 +1,13 @@
+﻿
+var leak = new Leaker();
+$("#start_button").click(function () {
+    if (leak !== null || leak !== undefined) {
+        return;
+    }
+    leak = new Leaker();
+    leak.init();
+});
+
+$("#destroy_button").click(function () {
+    leak.destroy();
+});
