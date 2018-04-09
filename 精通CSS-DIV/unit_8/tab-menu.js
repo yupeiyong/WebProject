2e9751c271e012971b9f3b0e6b87c66c8fc74b7d
yupeiyong <2758318132@qq.com>
2018-04-09 12:11:00 +0800

    add tab menu

---
+++ 精通CSS-DIV/unit_8/tab-menu.js
@@ -0,0 +1,22 @@
+﻿var containers = document.getElementsByClassName('container');
+if (containers.length > 0) {
+    var container = containers[0];
+    var id = container.attributes['id'];
+    var selector = '.nav li.' + id.nodeValue;
+    var li = container.querySelector(selector);
+    if (li) {
+        var liClass = li.attributes['class'];
+        if (liClass) {
+            var classValue = liClass.nodeValue;
+            if (classValue.length === 0) {
+                classValue = "on";
+            } else {
+                classValue += " on";
+            }
+            li.attributes['class'] = classValue;
+        } else {
+            li.attributes['class'] = "on";
+        }
+        //li.addClass(' on');
+    }
+}
