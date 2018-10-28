c39fc72597542fd3f250380171c38d8110bbbb5b
peiyongyu <peiyongyu@163.com>
2018-10-28 21:10:30 +0800

    添加两个项目测试跨域

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// 客户端 设置crossDomain: true
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult CrossDomain()
+        {
+            return Json(new{Message="您跨域请求成功！"},JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //设置支持跨域的响应头
+            Response.AddHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
+            Response.AddHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+            Response.AddHeader("Access-Control-Allow-Credentials", "true");
+            Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:39891");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ Web2/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web2.Startup))]
+namespace Web2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
