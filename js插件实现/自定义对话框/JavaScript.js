ed119a918104c3e2433cf45eb0a7bee455daedc3
yupeiyong <peiyongyu@163.com>
2018-11-11 22:32:44 +0800

    js plugin test

---
+++ js插件实现/自定义对话框/JavaScript.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict"
+    var _global;
+
+    var extend = function (original, newObj, isOverride) {
+        for (var key in newObj) {
+            if(newObj.hasOwnProperty(key) && (!newObj.hasOwnProperty(key)||isOverride )){
+                original[key]=newObj[key];
+            }
+        }
+        return original;
+    }
+    var dialog = function () {
+        this.init()
+    };
+
+    dialog.prototype = {
+        init: function () {
+
+        },
+        show: function () {
+            alert('show');
+        }
+    };
+    _global = (function () { return this || (0, eval)('this'); }());
+    !('dialog' in _global) && (_global.dialog=dialog);
+}());
